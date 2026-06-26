f4bd3b6827ec2d524ee8d2b5d73308683a4146cf
vindiputri <vindiputri9@gmail.com>
2026-06-26 16:18:45 +0700

    error routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/login', function () {
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
